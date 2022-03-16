d8d8e2c8ddfa889b74596911202c46b57df81ba1
tjdwhd70599@gmail.com <tjdwhd70599@gmail.com>
2022-03-16 18:44:54 +0900

    에러 정의

--- ConsoleApplication1/ConsoleApplication2.cpp
+++ ConsoleApplication1/ConsoleApplication2.cpp
@@ -1,22 +1,11 @@
 #include <cstdio>
+#include <Windows.h>
 
 int main()
 {
-    int nx = 36;
-    int ny = 0;
-    int* pn = &nx;
-
-    printf("Addr:%p\n", &nx);
-    printf("Addr:%p\n", &ny);
-    printf("Addr:%p\n", &pn);
-    printf("Size:%d\n", sizeof(pn));
-    printf("Addr:%p\n", &pn);
-    
-    ny = *pn;
-    *pn = ny+1;
-    nx = *pn + *(&ny);
-
-    printf("%d, %d, %d\n", nx, ny, *pn);
+    // $err,hr 을 Name에 입력
+    HANDLE hFile = CreateFile(TEXT("C:\\Jeff"), 0, 0, NULL, OPEN_EXISTING, 0, NULL);
+    GetLastError();
 
     return 0;
 }
