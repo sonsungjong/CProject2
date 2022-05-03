6e5a71c4d01ea63f4d227299e02bdddb20e9b9bf
tjdwhd70599@gmail.com <tjdwhd70599@gmail.com>
2022-05-03 17:27:51 +0900

    win api 타이머 한개

---
+++ WindowsApi2timer/WindowsApi2timer.cpp
@@ -0,0 +1,107 @@
+#include "framework.h"
+#include "WindowsApi2timer.h"
+
+HINSTANCE g_hInst;                                // current instance
+const TCHAR* g_class_name = _T("MyTimer__");
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    HDC hdc;
+    PAINTSTRUCT ps;
+    SYSTEMTIME st;
+    static TCHAR sTime[128];
+
+    switch (message)
+    {
+    case WM_TIMER:
+        GetLocalTime(&st);
+        _stprintf_s(sTime, 128, _T("지금 시간은 %02d:%02d:%02d입니다"), st.wHour, st.wMinute, st.wSecond);
+        InvalidateRect(hWnd, NULL, TRUE);
+        return 0;
+    case WM_COMMAND:
+    {
+        int wmId = LOWORD(wParam);
+        // Parse the menu selections:
+        switch (wmId)
+        {
+        case IDM_EXIT:
+            DestroyWindow(hWnd);
+            break;
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
+        }
+    }
+    break;
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+        // TODO: Add any drawing code that uses hdc here...
+        TextOut(hdc, 100, 100, sTime, (int)_tcslen(sTime));
+        EndPaint(hWnd, &ps);
+    }
+    break;
+
+    case WM_DESTROY:
+        KillTimer(hWnd, 1);
+        PostQuitMessage(0);
+        break;
+    }
+    return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    WNDCLASSEXW wcex;
+    const TCHAR* class_name = TEXT("myClassNameTimer");
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINDOWSAPI2TIMER));
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_WINDOWSAPI2TIMER);
+    wcex.lpszClassName = class_name;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+    RegisterClassEx(&wcex);
+
+    g_hInst = hInstance; // Store instance handle in our global variable
+
+    HWND hWnd = CreateWindowW(class_name, class_name, WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+    if (!hWnd)
+    {
+        return FALSE;
+    }
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WINDOWSAPI2TIMER));
+
+    MSG msg;
+    SetTimer(hWnd, 1, 1000, NULL);
+
+    // Main message loop:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+    return (int) msg.wParam;
+}
