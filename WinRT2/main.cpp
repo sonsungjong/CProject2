8d74114daf09fb2649a8d3a1367e8f0c866986d7
tjdwhd70599@gmail.com <tjdwhd70599@gmail.com>
2022-10-20 06:47:48 +0900

    WinRT 머신러닝

---
+++ WinRT2/main.cpp
@@ -0,0 +1,82 @@
+﻿#include "pch.h"
+
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace Windows::AI::MachineLearning;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::Graphics::Imaging;
+using namespace Windows::Media;
+using namespace Windows::Storage;
+
+using namespace std;
+
+// Global variables
+hstring modelPath;
+string deviceName = "default";
+hstring imagePath;
+LearningModel model = nullptr;
+LearningModelDeviceKind deviceKind = LearningModelDeviceKind::Default;
+LearningModelSession session = nullptr;
+LearningModelBinding binding = nullptr;
+VideoFrame imageFrame = nullptr;
+string labelsFilePath;
+vector<string> labels;
+
+// Forward declarations
+void LoadModel();
+VideoFrame LoadImageFile(hstring filePath);
+void BindModel();
+void EvaluateModel();
+void PrintResults(IVectorView<float> results);
+void LoadLabels();
+
+int main()
+{
+    init_apartment();
+    modelPath = L"C:\\Repos\\Windows-Machine-Learning\\SharedContent\\models\\SqueezeNet.onnx";
+    imagePath = L"C:\\Repos\\Windows-Machine-Learning\\SharedContent\\media\\kitten_224.png";
+    LoadModel();
+    imageFrame = LoadImageFile(imagePath);
+}
+
+void LoadModel()
+{
+    // load the model
+    printf("Loading modelfile '%ws' on the '%s' device\n", modelPath.c_str(), deviceName.c_str());
+    DWORD ticks = GetTickCount();
+    model = LearningModel::LoadFromFilePath(modelPath);
+    ticks = GetTickCount() - ticks;
+    printf("model file loaded in %d ticks\n", ticks);
+}
+
+VideoFrame LoadImageFile(hstring filePath)
+{
+    printf("Loading the image...\n");
+    DWORD ticks = GetTickCount();
+    VideoFrame inputImage = nullptr;
+
+    try
+    {
+        // open the file
+        StorageFile file = StorageFile::GetFileFromPathAsync(filePath).get();
+        // get a stream on it
+        auto stream = file.OpenAsync(FileAccessMode::Read).get();
+        // Create the decoder from the stream
+        BitmapDecoder decoder = BitmapDecoder::CreateAsync(stream).get();
+        // get the bitmap
+        SoftwareBitmap softwareBitmap = decoder.GetSoftwareBitmapAsync().get();
+        // load a videoframe from it
+        inputImage = VideoFrame::CreateWithSoftwareBitmap(softwareBitmap);
+    }
+    catch (...)
+    {
+        printf("failed to load the image file, make sure you are using fully qualified paths\r\n");
+        exit(EXIT_FAILURE);
+    }
+
+    ticks = GetTickCount() - ticks;
+    printf("image file loaded in %d ticks\n", ticks);
+    // all done
+    return inputImage;
+}
+
