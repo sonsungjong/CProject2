a7b6731c86a714d87f2497596942a67fa30881d0
tjdwhd70599@gmail.com <tjdwhd70599@gmail.com>
2022-10-27 13:01:36 +0900

    MySQL 64bit

---
+++ MySQL64/MySQL64.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <Windows.h>
+#include <mysql.h>
+
+#define HOST            "127.0.0.1"
+#define DBUser              "root"
+#define DBPass              "root"
+#define DBName              "company"
+#define DBPORT              3306
+
+int main()
+{
+    MYSQL conn;
+    MYSQL* mysqlConn;
+    mysql_init(&conn);
+    //mysqlConn = mysql_real_connect(&conn, "127.0.0.1", "root", "root", "company", 3306, NULL, 0);
+    mysqlConn = mysql_real_connect(&conn, HOST, DBUser, DBPass, DBName, DBPORT, NULL, 0);
+    if (mysqlConn != NULL) {
+        std::cout << "Connected";
+    }
+    else {
+        std::cout << "Not Connected";
+    }
+}
