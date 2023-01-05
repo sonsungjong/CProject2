1b1cce78133cb55a4b2529d9bd35388e20b94167
sonsungjong <tjdwhd70599@gmail.com>
2023-01-05 13:47:32 +0900

    CRUD 추가

--- ODBC_MARIA2/ODBC_MARIA2.cpp
+++ ODBC_MARIA2/ODBC_MARIA2.cpp
@@ -6,16 +6,17 @@
 #include <sal.h>
 #include <stdlib.h>
 // ODBC
-#include <sqltypes.h>
-#include <sql.h>
+#include <odbcinst.h>
 #include <sqlext.h>
+#include <sql.h>
+#include <sqltypes.h>
 
 // ODBC 드라이버 설치
 // 제어판 -> 관리 도구 -> ODBC Data Sources (64 or 32) -> 사용자 DSN '추가' -> MariaDB ODBC 3.1 Driver
 // DataSource Name : ODBC등록명, TCP/IP : IP주소, User : 아이디, Password : 비밀번호, Database : 스키마명
 
-SQLHENV hEnv;
-SQLHDBC hDbc;
+SQLHENV hEnv;                   // ODBC 환경 정보
+SQLHDBC hDbc;                   // ODBC 함수 정보
 SQLHSTMT hStmt;
 
 SQLTCHAR* ODBC_Name = (SQLTCHAR*)_T("employee_maria");                    // ODBC등록명
@@ -32,6 +33,7 @@ bool DBConnect()
         return false;
     }
     if (SQLSetEnvAttr(hEnv, SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3, SQL_IS_INTEGER) != SQL_SUCCESS) {
+        // ODBC 버전 정보
         return false;
     }
     if (SQLAllocHandle(SQL_HANDLE_DBC, hEnv, &hDbc) != SQL_SUCCESS) {
@@ -106,18 +108,43 @@ bool Select(const TCHAR* a_where)
 bool Insert(const TCHAR* a_name, const TCHAR* a_gender)
 {
     // INSERT INTO employee (emp_name, emp_gender) VALUES ('쿼리입력','woman');
-
+    TCHAR sql_query[255];
+    memset(sql_query, 0, 255);
+    _stprintf_s(sql_query, 255, _T("INSERT INTO employee (emp_name, emp_gender) VALUES (\'%s\', \'%s\')"), a_name, a_gender);
+    RETCODE ret = SQLExecDirect(hStmt, (SQLTCHAR*)sql_query, SQL_NTS);
+    if (ret == SQL_SUCCESS || ret == SQL_SUCCESS_WITH_INFO) {}
+    else { return false; }
+    SQLEndTran(SQL_HANDLE_ENV, hEnv, SQL_COMMIT);
+    if (hStmt != NULL) { SQLCloseCursor(hStmt); }
+    return true;
 }
 
 bool Update(const long long a_no, const TCHAR* a_name)
 {
     // UPDATE employee SET emp_no=%d WHERE emp_name='변경이름';
-
+    TCHAR sql_query[255];
+    memset(sql_query, 0, 255);
+    _stprintf_s(sql_query, 255, _T("UPDATE employee SET emp_name=\'%s\' WHERE emp_no=%lld"), a_name, a_no);
+    RETCODE ret = SQLExecDirect(hStmt, (SQLTCHAR*)sql_query, SQL_NTS);
+    if (ret == SQL_SUCCESS || ret == SQL_SUCCESS_WITH_INFO) {}
+    else { return false; }
+    SQLEndTran(SQL_HANDLE_ENV, hEnv, SQL_COMMIT);
+    if (hStmt != NULL) { SQLCloseCursor(hStmt); }
+    return true;
 }
 
 bool Delete(const long long a_empno)
 {
     // DELETE FROM employee WHERE emp_no=3;
+    TCHAR sql_query[255];
+    memset(sql_query, 0, 255);
+    _stprintf_s(sql_query, 255, _T("DELETE FROM employee WHERE emp_no=\'%lld\'"), a_empno);
+    RETCODE ret = SQLExecDirect(hStmt, (SQLTCHAR*)sql_query, SQL_NTS);
+    if (ret == SQL_SUCCESS || ret == SQL_SUCCESS_WITH_INFO) {}
+    else { return false; }
+    SQLEndTran(SQL_HANDLE_ENV, hEnv, SQL_COMMIT);
+    if (hStmt != NULL) { SQLCloseCursor(hStmt); }
+    return true;
 }
 
 int main()
@@ -132,40 +159,48 @@ int main()
         return 1;
     }
 
-    int choice = 0;
-    printf("1:Select, 2:Insert, 3:Update, 4:Delete, 5:SelectAll, 0:종료\n");
-    scanf_s("%d", &choice);
+    while(true){
+        int choice = 0;
+        printf("1:Select, 2:Insert, 3:Update, 4:Delete, 5:SelectAll, 0:종료\n");
+        scanf_s("%d", &choice);
     
-    if (choice == 0) {
-        /* no actions */
-    }
-    else if (choice == 1) {
-        if (Select(_T("유관순")) == false) {
-            printf("Select 실패\n");
+        if (choice == 0) {
+            /* no actions */
+            break;
         }
-    }
-    else if (choice == 2) {
-        if (Insert(_T("짜장이"), _T("남자")) == false) {
-            printf("Insert 실패\n");
+        else if (choice == 1) {
+            if (Select(_T("유관순")) == false) {
+                printf("Select 실패\n");
+            }
         }
-    }
-    else if (choice == 3) {
-        if (Update(4, _T("이름변경")) == false) {
-            printf("Update 실패\n");
+        else if (choice == 2) {
+            if (Insert(_T("이름입력"), _T("남자")) == false) {
+                printf("Insert 실패\n");
+            }
         }
-    }
-    else if (choice == 4) {
-        if (Delete(9) == false) {
-            printf("Delete 실패\n");
+        else if (choice == 3) {
+            printf("변경할 번호 입력: ");
+            scanf_s("%d", &choice);
+            if (Update(choice, _T("이름변경")) == false) {
+                printf("Update 실패\n");
+            }
         }
-    }
-    else if (choice == 5) {
-        if (SelectAll() == false) {
-            printf("SelectAll 실패\n");
+        else if (choice == 4) {
+            printf("삭제할 번호 입력: ");
+            scanf_s("%d", &choice);
+            if (Delete(choice) == false) {
+                printf("Delete 실패\n");
+            }
         }
-    }
-    else {
-        /* no actions */
+        else if (choice == 5) {
+            if (SelectAll() == false) {
+                printf("SelectAll 실패\n");
+            }
+        }
+        else {
+            /* no actions */
+            printf("없는 번호 입니다.\n");
+        }
     }
 
     DBDisConnect();
