08b1263c40ba457d7733de39f7a616b90268c41b
sonsungjong <tjdwhd70599@gmail.com>
2023-04-14 11:32:41 +0900

    UTF-8 csv파일을 열어 TCHAR 문자열에 저장

--- csv_read/csv_read.cpp
+++ csv_read/csv_read.cpp
@@ -1,18 +1,22 @@
 // csv파일을 읽어서 내용을 출력하는 예제
 
-// 튜플 500개까지
-#define MAX_COUNT               500
+// 튜플 200개까지
+#define MAX_COUNT               200
 #include <cstdio>               // printf, fopen_s, fgets, fclose
 #include <cstdlib>              // atoi
+#include <tchar.h>
+#include <locale>
+#include <codecvt>
+#include <Windows.h>
 
 typedef struct VisitorData
 {
-    char data_str[12];                  // 날짜 정보
-    char day_of_week[4];            // 요일정보
+    TCHAR data_str[12];                  // 날짜 정보
+    TCHAR day_of_week[4];            // 요일정보
     unsigned int count;                 // 방문 횟수
 } VD;
 
-char* GetNextString(char* ap_src_str, char a_delimiter, char* ap_buffer)
+TCHAR* GetNextString(TCHAR* ap_src_str, TCHAR a_delimiter, TCHAR* ap_buffer)
 {
     // NULL 또는 a_delimiter 가 나올때까지
     while (*ap_src_str && *ap_src_str != a_delimiter)
@@ -28,49 +32,57 @@ char* GetNextString(char* ap_src_str, char a_delimiter, char* ap_buffer)
     return ap_src_str;
 }
 
+
+
 int main()
 {
-    FILE* p_file = NULL;                // 파일을 열어서 사용할 포인터
-    char one_line_string[128] = { 0, }, str[32] = { 0, }, *p_pos;
+    // 한글 wprintf 사용
+    _tsetlocale(0, _T("korean"));
+
+    TCHAR one_line_string[128] = { 0, }, str[32] = { 0, }, *p_pos;
     int data_count = 0;             // 전체 데이터 갯수
     int total_visitors = 0, i = 0;
+    VD data[MAX_COUNT] = { 0, };
+    FILE* p_file = NULL;                // 파일을 열어서 사용할 포인터
 
-    //if (0 == fopen_s(&p_file, "D:\\tipsware.csv", "rt"))             // 파일을 연다 (성공하면 0 반환)
-    if (0 == fopen_s(&p_file, "..\\data.csv", "rt"))             // 파일을 연다 (성공하면 0 반환)
+    //if (0 == fopen_s(&p_file, "D:\\euckr.csv", "rt"))             // 파일을 연다 (성공하면 0 반환)
+    if (0 == _tfopen_s(&p_file, _T("..\\data.csv"), _T("rt, ccs=UTF-8")))             // 파일을 연다 (성공하면 0 반환, UTF-8 파일을 열기)
     {
         printf("파일 열기 성공 \n");
 
-        VD data[MAX_COUNT] = { 0, };
-
         // 파일의 첫 7줄을 건너 뛴다 (전처리)
         for (int i = 0; i < 7; ++i) {
-            fgets(one_line_string, 128, p_file);            // 한줄을 읽는다
+            _fgetts(one_line_string, sizeof(one_line_string)/sizeof(TCHAR), p_file);            // 한줄 읽기
         }
 
-        while (fgets(one_line_string, 128, p_file))                 // 정보를 읽는다
+        // 정보를 읽는다 (추출 대상)
+        while (_fgetts(one_line_string, 128, p_file))
         {
             // 시작이 , 로 시작한다면 데이터가 끝났다는 것
             if (one_line_string[0] == ',') { break; }
+
+            // one_line_string의 문자열 형태를 변환한다(from utf-8 to utf-16 le)
+            //MultiByteToWideChar(CP_UTF8, 0, char_one_line_string, -1, one_line_string, sizeof(one_line_string)/sizeof(TCHAR));                // 성공하면 1, 실패하면 0
 
             // , 를 기준으로 문자열을 분리한다
             p_pos = one_line_string;
             p_pos = GetNextString(p_pos, ',', data[data_count].data_str);
             p_pos = GetNextString(p_pos, ',', data[data_count].day_of_week);
             p_pos = GetNextString(p_pos, ',', str);
-            data[data_count].count = atoi(str);
+            data[data_count].count = _ttoi(str);
 
             data_count++;               // 데이터 갯수 증가
         }
 
         while (i < data_count) {
-            printf("%s [%s] : %4d, ", data[i].data_str, data[i].day_of_week, data[i].count);
+            _tprintf(_T("%s [%s] : %4d, "), data[i].data_str, data[i].day_of_week, data[i].count);
             total_visitors += data[i].count;
             if (!(++i % 4)) { printf("\n"); }               // 4개마다 줄바꿈
         }
-        printf("\n\n%s ~ %s 까지의 일일 블로그 방문 횟수입니다.\n", data[0].data_str, data[data_count-1].data_str);
+        _tprintf(_T("\n\n%s ~ %s 까지의 일일 블로그 방문 횟수입니다.\n"), data[0].data_str, data[data_count-1].data_str);
 
         if (data_count > 0) {
-            printf("이 기간의 총 방문 횟수는 %d회이고 일일 평균 방문 횟수는 %d회 입니다.\n", total_visitors, total_visitors / data_count);
+            _tprintf(_T("이 기간의 총 방문 횟수는 %d회이고 일일 평균 방문 횟수는 %d회 입니다.\n"), total_visitors, total_visitors / data_count);
         }
 
         fclose(p_file);         // 파일을 닫는다
