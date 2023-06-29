0957b403ed3736bd2c2d8b1598e6914d2968c58b
sonsungjong <tjdwhd70599@gmail.com>
2023-06-29 09:42:58 +0900

    테스트 콘솔

---
+++ TestConsole/TestConsole.cpp
@@ -0,0 +1,16 @@
+﻿#include <cstdio>
+#include <chrono>
+#include <thread>
+
+int main()
+{
+    unsigned int flag = 0;
+
+    while (flag < 4294967294) {
+        printf("hello docker\n");
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        ++flag;
+    }
+
+    return 0;
+}
