49db4acce1105f43de9d63290370e5d8dea5afa3
sonsungjong <tjdwhd70599@gmail.com>
2023-12-22 11:01:51 +0900

    ini file

---
+++ IniFile/IniFile.cpp
@@ -0,0 +1,75 @@
+// ini 파일을 읽고 쓰는 예제
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <string>
+
+class IniFile
+{
+public:
+    IniFile() : m_username(""), m_password("")
+    {
+        m_ini_path = ".\\Config.ini";
+    }
+
+    void readINI()
+    {
+        std::ifstream file(m_ini_path);
+        std::string line;
+
+        if (file.is_open() == false)
+        {
+            std::cerr << "Unable to open file" << std::endl;
+            return;
+        }
+
+        while (std::getline(file, line))
+        {
+            std::istringstream is_line(line);
+            std::string key;
+            if (std::getline(is_line, key, '='))
+            {
+                std::string value;
+                if (std::getline(is_line, value))
+                {
+                    if (key == "username") { 
+                        m_username = value; 
+                    }
+                    else if (key == "password") {
+                        m_password = value;
+                    }
+                    else {
+                        /* no actions */
+                    }
+                }
+            }
+        }
+    }
+
+    std::string getUsername()
+    {
+        return m_username;
+    }
+
+    std::string getPassword()
+    {
+        return m_password;
+    }
+
+private:
+    std::string m_ini_path;
+    std::string m_username;
+    std::string m_password;
+};
+
+int main()
+{
+    IniFile* ini_file = new IniFile;
+    ini_file->readINI();
+
+    std::cout << ini_file->getUsername() << std::endl;
+    std::cout << ini_file->getPassword() << std::endl;
+
+    delete ini_file;
+}
