a32f561f5cf10a67d0ac7325821c2a07366120f7
tjdwhd70599@gmail.com <tjdwhd70599@gmail.com>
2024-01-07 17:52:06 +0900

    복제 생성자와 복제 대입 연산자

--- ProfessionalCpp/SpreadsheetCell.cpp
+++ ProfessionalCpp/SpreadsheetCell.cpp
@@ -1,8 +1,14 @@
 module spreadsheet_cell;
 import <charconv>;
+import <iostream>;
 using namespace std;
 
-void SpreadsheetCell::setValue(double value) 
+SpreadsheetCell::~SpreadsheetCell()
+{
+	printf("Destructor called.\n");
+}
+
+void SpreadsheetCell::setValue(double value)
 {
 	m_value = value;
 }
@@ -12,7 +18,7 @@ double SpreadsheetCell::getValue() const
 	return m_value;
 }
 
-void SpreadsheetCell::setString(string_view value) 
+void SpreadsheetCell::setString(std::string_view value) 
 {
 	m_value = stringToDouble(value);
 }
@@ -27,7 +33,7 @@ string SpreadsheetCell::doubleToString(double value) const
 	return to_string(value);
 }
 
-double SpreadsheetCell::stringToDouble(string_view value) const
+double SpreadsheetCell::stringToDouble(std::string_view value) const
 {
 	double number { 0 };
 	from_chars(value.data(), value.data() + value.size(), number);
@@ -39,9 +45,23 @@ SpreadsheetCell::SpreadsheetCell(double initialValue)
 	setValue(initialValue);
 }
 
-SpreadsheetCell::SpreadsheetCell(string_view initialValue)
+SpreadsheetCell::SpreadsheetCell(std::string_view initialValue)
+	: SpreadsheetCell {stringToDouble(initialValue)}
 {
-	setString(initialValue);
+	//setString(initialValue);				// 위임생성자로 대체 (생성자에서 다른 생성자 호출)
+}
+
+SpreadsheetCell& SpreadsheetCell::operator=(const SpreadsheetCell& rhs)
+{
+	if (this == &rhs) 
+	{
+		// 자기 자신을 대입할 경우
+		return *this;
+	}
+	else {
+		m_value = rhs.m_value;					// 값을 복사
+		return *this;
+	}
 }
 
 SpreadsheetCell::SpreadsheetCell()
@@ -49,8 +69,50 @@ SpreadsheetCell::SpreadsheetCell()
 	m_value = 0;
 }
 
-int main()
+class EvenSequence8
 {
+public:
+	EvenSequence8(std::initializer_list<double> args) 
+	{
+		if (args.size() % 2 != 0)
+		{
+			throw std::invalid_argument { "initializer_list should "
+				"contain even number of elements." };
+		}
+		m_sequence.reserve(args.size());
+		for (const auto& value : args)
+		{
+			m_sequence.push_back(value);
+		}
+	}
+
+	void dump() const
+	{
+		for (const auto& value : m_sequence)
+		{
+			printf("%lf, ", value);
+		}
+		printf("\n");
+	}
+
+private:
+	std::vector<double> m_sequence;
+};
+
+int mainChapter8()
+{
+	SpreadsheetCell myCell1{ 4 };
+	SpreadsheetCell myCell2{ myCell1 };				// 복제
+	
+	// 스마트 포인터를 사용하지 않은 프리스토어 객체는 명시적으로 delete를 호출해서 메모리를 해제해야한다.
+	SpreadsheetCell* cellPtr1{ new SpreadsheetCell {5} };
+	printf("cellPtr1: %lf\n", cellPtr1->getValue());
+	delete cellPtr1;
+	cellPtr1 = nullptr;			// 해제 후 빈값 대입
+
+	// 객체에 대입
+	SpreadsheetCell myCell3{ 5 }, anotherCell;
+	anotherCell = myCell3;
 
 
 	return 0;
--- ProfessionalCpp/SpreadsheetCell.cppm
+++ ProfessionalCpp/SpreadsheetCell.cppm
@@ -1,13 +1,19 @@
 export module spreadsheet_cell;
 import <string>;
 import <string_view>;
+import <vector>;
 
 export class SpreadsheetCell
 {
 public:
+	//SpreadsheetCell(const SpreadsheetCell& src) = delete;			// 복제 생성자 삭제
+	SpreadsheetCell(const SpreadsheetCell& src) = default;			// 복제 생성자 (명시)
 	SpreadsheetCell(double initialValue);
-	SpreadsheetCell(std::string_view initialValue);
-	SpreadsheetCell();
+	explicit SpreadsheetCell(std::string_view initialValue);				// 암묵적 변환이 필요한 경우가 아니라면 단일 매개변수를 받는 생성자는 explicit 으로 지정한다
+	SpreadsheetCell() = default;
+	SpreadsheetCell& operator=(const SpreadsheetCell& rhs);				// 디폴트 대입 연산자
+
+	~SpreadsheetCell();				// 소멸자
 
 	void setValue(double value);
 	double getValue() const;
