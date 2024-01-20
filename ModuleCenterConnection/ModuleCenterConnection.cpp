3ec8130b23fa7c0438defb2eef6b94527ac57e3e
tjdwhd70599@gmail.com <tjdwhd70599@gmail.com>
2024-01-20 15:01:23 +0900

    중앙관리자 멤버변수 여러개로 1차 완료

--- ModuleCenterConnection/ModuleCenterConnection.cpp
+++ ModuleCenterConnection/ModuleCenterConnection.cpp
@@ -16,88 +16,56 @@
 #include <tuple>
 #include <variant>
 
-// 중앙 서비스는 싱글턴패턴으로 모든 클래스의 모든 메서드들을 등록해놓는다
-// 구현 중 필요에 따라서 각 모듈의 기능에 다른 클래스의 메서드를 삽입할 수 있도록 CenterService를 통해 주입한다.
-
-enum enumCenterService
-{
-    NONE = 0,
-    METHOD1 = 1,
-    METHOD2 = 2,
-    METHOD3 = 3,
-    METHOD4 = 4,
-};
-
-
 class CenterService
 {
 public:
-    using T = std::variant <
-        std::function<std::string(int)>,
-        std::function<int(std::string)>,
-        std::function<void(void)>,
-        std::function<void(int, double)>
-    >;
-
-    void registerFunction(int key, std::function<std::string(int)> func)
+    enum enumCenterService
     {
-        m_functions[key] = func;
+        NONE = 0,
+        METHOD1 = 1,
+        METHOD2 = 2,
+        METHOD3 = 3,
+        METHOD4 = 4,
+    };
+
+    void registerFunction(int key, const std::function<std::string(int)> func)
+    {
+        m_getStr_setInt[key] = func;
     }
 
-    void registerFunction(int key, std::function<int(std::string)> func)
+    void registerFunction(int key, const std::function<int(std::string)> func)
     {
-        m_functions[key] = func;
+        m_getInt_setStr[key] = func;
     }
 
     void registerFunction(int key, std::function<void(void)> func)
     {
-        m_functions[key] = func;
+        m_getVoid_setVoid[key] = func;
     }
 
-    //void registerFunction(int key, std::function<void(int, double)> func)
-    //{
-    //    m_functions[key] = func;
-    //}
+    void registerFunction(int key, std::function<void(int, double)> func)
+    {
+        m_getVoid_setIntDouble[key] = func;
+    }
 
     std::string useFunction(int key, int num)
     {
-        auto it = m_functions.find(key);
-        if (it != m_functions.end())
-        {
-            auto& func = it->second;
-            if (auto strFunc = std::get_if<std::function<std::string(int)>>(&func))
-            {
-                return (*strFunc)(num);
-            }
-        }
-        return "";
+        return m_getStr_setInt[key](num);
     }
 
     int useFunction(int key, std::string str)
     {
-        auto it = m_functions.find(key);
-        auto& func = it->second;
-        if (auto intFunc = std::get_if<std::function<int(std::string)>>(&func))
-        {
-            return (*intFunc)(str);
-        }
-        return 0;
+        return m_getInt_setStr[key](str);
     }
 
     void useFunction(int key)
     {
-        auto it = m_functions.find(key);
-        auto& func = it->second;
-        auto voidFunc = std::get_if<std::function<void(void)>>(&func);
-        (*voidFunc)();
+        return m_getVoid_setVoid[key]();
     }
 
     void useFunction(int key, int num, double dbNum)
     {
-        auto it = m_functions.find(key);
-        auto& func = it->second;
-        auto voidFunc = std::get_if<std::function<void(int, double)>>(&func);
-        (*voidFunc)(num, dbNum);
+        return m_getVoid_setIntDouble[key](num, dbNum);
     }
 
     // 싱글턴구성
@@ -112,7 +80,12 @@ private:
     CenterService() {}          // 싱글턴구성
 
 private:
-    std::unordered_map<int, T> m_functions;
+    std::function<std::string(int)> m_getStr_setInt[255];
+    std::function<int(std::string)> m_getInt_setStr[255];
+    std::function<void(void)> m_getVoid_setVoid[255];
+    std::function<void(int, double)> m_getVoid_setIntDouble[255];
+
+    
 };
 
 class Class1
@@ -129,7 +102,7 @@ class Class2
 {
 public:
     int method2(std::string str) {
-        printf("method2 사용 %s\n", str.c_str());
+        printf("method2 사용 : %s\n", str.c_str());
         return 2;
     }
 };
@@ -144,32 +117,34 @@ public:
     }
 
     void method4(int num, double dbNum) {
-        printf("method4 사용 \n");
+        printf("method4 사용 : %d, %lf \n", num, dbNum);
     }
 };
 
 int main()
 {
     Class1* p_class1 = new Class1;
-    Class2* p_class2 = new Class2;
+    Class2 class2;
     Class3* p_class3 = new Class3;
 
     CenterService& service = CenterService::getInstance();
 
-    service.registerFunction(1, std::bind(&Class1::method1, p_class1, std::placeholders::_1));
-    service.registerFunction(2, std::bind(&Class2::method2, p_class2, std::placeholders::_1));
-    service.registerFunction(3, std::bind(&Class3::method3, p_class3));
-    //service.registerFunction(4, std::bind(&Class3::method4, p_class3, std::placeholders::_1, std::placeholders::_2));
+    service.registerFunction(CenterService::METHOD1, std::function<std::string(int)>(std::bind(&Class1::method1, p_class1, std::placeholders::_1)));
+    service.registerFunction(CenterService::METHOD2, std::function<int(std::string)>(std::bind(&Class2::method2, &class2, std::placeholders::_1)));
+    service.registerFunction(CenterService::METHOD3, std::function<void(void)>(std::bind(&Class3::method3, p_class3)));
+    service.registerFunction(CenterService::METHOD4, std::function<void(int, double)>(std::bind(&Class3::method4, p_class3, std::placeholders::_1, std::placeholders::_2)));
 
     std::string str = "hello world";
 
-    std::string result1 = service.useFunction(1, 123123);
-    int result2 = service.useFunction(2, str);
-    service.useFunction(3);
-    //service.useFunction(4, 444, 4.14);
+    std::string result1 = service.useFunction(CenterService::METHOD1, 123123);
+    int result2 = service.useFunction(CenterService::METHOD2, str);
+
+    printf("main 리턴 결과 : %s, %d\n", result1.c_str(), result2);
+
+    service.useFunction(CenterService::METHOD3);
+    service.useFunction(CenterService::METHOD4, 444, 4.14);
 
     delete p_class3;
-    delete p_class2;
     delete p_class1;
 }
 
