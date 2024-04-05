9ce4680dc438fdbaae2bdf2e122d3f1cc7250300
sonsungjong <tbxmtbfm@naver.com>
2024-04-05 16:00:23 +0900

    배속재생 완료

--- DoubleBufferFileReplay/DoubleBufferFileReplay.cpp
+++ DoubleBufferFileReplay/DoubleBufferFileReplay.cpp
@@ -47,7 +47,7 @@ public:
     long long m_startTime;                         // 수신을 시작한 ms시간 (나중에 수신종료 명령시 현재시간에서 startTime을 빼서 playTime을 기록)
     long long m_endTime;
     long long m_playTime;                       // 파일에 기록할 총 경과시간, 마지막에 모든 버퍼를 파일로 저장하고 맨 앞에 기록한다, ms 단위
-    //std::atomic<long long> m_timerTime;               // 타이머의 경과시간 (30ms 단위, 30Hz이상)
+    //std::atomic<long long> m_timerTime;               // 타이머의 경과시간 (32ms 단위, 약 30Hz)
     std::atomic<bool> m_endFlag;                               // 종료 플래그
     std::mutex m_mutex;             // 버퍼 스왑을 위한 뮤텍스
     std::condition_variable m_cv;
@@ -78,7 +78,7 @@ public:
             m_buffer2.swap(emptyQueue2);
         }
         m_curBufferSize = 0;
-        m_endFlag.store(false);
+        m_endFlag.store(true);
 
         mp_currentBuffer = &m_buffer1;
         mp_processingBuffer = &m_buffer2;
@@ -91,73 +91,90 @@ public:
 
     void startSave()
     {
-        m_startTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-        // 폴더 및 파일을 생성하고 맨 앞 부분에 훈련시간을 8바이트만큼 0으로 기록한다 (경로에 폴더가 없으면 생성, 기존에 파일이 있으면 기존거 삭제)
-        m_playTime = m_endTime - m_startTime;
-        std::string filename = "C:\\data\\scenario_" + std::to_string(m_startTime) + ".dat";
-        std::ofstream timeWriter(filename.c_str());
-        if (!timeWriter.is_open()) {
-            printf("Failed to open file\n");
-        }
-        else {
-            timeWriter.write(reinterpret_cast<const char*>(&m_playTime), sizeof(m_playTime));               // 일단 0으로 기록해놓기
-            if (!timeWriter)
-            {
-                printf("Failed to write data\n");
+        if (m_endFlag == true)
+        {
+            m_endFlag.store(false);
+            m_startTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+            // 폴더 및 파일을 생성하고 맨 앞 부분에 훈련시간을 8바이트만큼 0으로 기록한다 (경로에 폴더가 없으면 생성, 기존에 파일이 있으면 기존거 삭제)
+            m_playTime = m_endTime - m_startTime;
+            std::string filename = "C:\\data\\scenario_" + std::to_string(m_startTime) + ".dat";
+            std::ofstream timeWriter(filename.c_str());
+            if (!timeWriter.is_open()) {
+                printf("Failed to open file\n");
+            }
+            else {
+                timeWriter.write(reinterpret_cast<const char*>(&m_playTime), sizeof(m_playTime));               // 일단 0으로 기록해놓기
+                if (!timeWriter)
+                {
+                    printf("Failed to write data\n");
+                }
+
+                timeWriter.close();
             }
 
-            timeWriter.close();
+            // recv를 시작한다 (==> Queue 저장로직을 연다)
+            m_addBufThread = std::thread([this] {this->addBufTimer(); });
+            m_addBufThread.detach();
+            printf("플레이 시작!\n");
         }
-
-        // recv를 시작한다 (==> Queue 저장로직을 연다)
-        m_addBufThread = std::thread([this] {this->addBufTimer(); });
-        m_addBufThread.detach();
+        else
+        {
+            printf("플레이가 이미 실행 중\n");
+        }
     }
 
     // Recv중지 명령 받음
     void endSave() {
         // recv 멈추고
-        m_endFlag.store(true);
-
-        // end_time 계산하고
-        m_endTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-
-        // m_processingBuffer가 모두 비워지는 것을 기다린다음
+        if (m_endFlag == false)
         {
-            std::unique_lock<std::mutex> lock(m_mutex);
-            m_cv.wait(lock, [this]() {return mp_processingBuffer->empty(); });
-        }
+            m_endFlag.store(true);
 
-        // m_currentBuffer를 모두 파일에 기록하고
-        if (!mp_currentBuffer->empty())
-        {
-            swapBuffer();
-            // 다시 m_processingBuffer가 모두 비워지는 것을 기다린다음
-            std::unique_lock<std::mutex> lock(m_mutex);
-            m_cv.wait(lock, [this]() {return mp_processingBuffer->empty(); });
-        }
+            // end_time 계산하고
+            m_endTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 
-        // m_playTime으로 맨 앞 8바이트를 덮어쓴다
-        m_playTime = m_endTime - m_startTime;
-        std::string filename = "C:\\data\\scenario_" + std::to_string(m_startTime) + ".dat";
-        std::ofstream timeWriter(filename.c_str(), std::ios::in | std::ios::out | std::ios::binary);
-        if (!timeWriter.is_open()) {
-            printf("Failed to open file\n");
-        }
-        else {
-            timeWriter.seekp(0, std::ios::beg);
-            timeWriter.write(reinterpret_cast<const char*>(&m_playTime), sizeof(m_playTime));
-            if (!timeWriter)
+            // m_processingBuffer가 모두 비워지는 것을 기다린다음
             {
-                printf("Failed to write data\n");
-                return;
-            }
-            else 
-            {
-                printf("총 플레이타임: %lld\n", m_playTime);
+                std::unique_lock<std::mutex> lock(m_mutex);
+                m_cv.wait(lock, [this]() {return mp_processingBuffer->empty(); });
             }
 
-            timeWriter.close();
+            // m_currentBuffer를 모두 파일에 기록하고
+            if (!mp_currentBuffer->empty())
+            {
+                swapBuffer();
+                // 다시 m_processingBuffer가 모두 비워지는 것을 기다린다음
+                std::unique_lock<std::mutex> lock(m_mutex);
+                m_cv.wait(lock, [this]() {return mp_processingBuffer->empty(); });
+            }
+
+            // m_playTime으로 맨 앞 8바이트를 덮어쓴다
+            m_playTime = m_endTime - m_startTime;
+            std::string filename = "C:\\data\\scenario_" + std::to_string(m_startTime) + ".dat";
+            std::ofstream timeWriter(filename.c_str(), std::ios::in | std::ios::out | std::ios::binary);
+            if (!timeWriter.is_open()) {
+                printf("Failed to open file\n");
+            }
+            else {
+                timeWriter.seekp(0, std::ios::beg);
+                timeWriter.write(reinterpret_cast<const char*>(&m_playTime), sizeof(m_playTime));
+                if (!timeWriter)
+                {
+                    printf("Failed to write data\n");
+                    return;
+                }
+                else 
+                {
+                    printf("총 플레이타임: %lld\n", m_playTime);
+                }
+
+                timeWriter.close();
+            }
+            printf("플레이가 정상 중지\n");
+        }
+        else 
+        {
+            printf("플레이가 실행중이 아님\n");
         }
 
     }
@@ -244,11 +261,12 @@ public:
     long long m_endTime;
     long long m_totalPlayTime;                       // 파일의 header (총 재현시간 ms단위, 파일 맨앞에 있음)
     long long m_curPlayTime;                              // 경과시간 (ms)
-    std::atomic<bool> m_endFlag;                        // 종료 플래그
+    std::atomic<int> m_endFlag;                        // 종료 플래그,, [0]시작, [1]중지, [2]일시정지
     std::mutex m_mutex;                             // 버퍼 스왚을 위한 뮤택스
     std::condition_variable m_cv;
     std::string m_filePath;                         // 대상 파일경로
     std::ifstream m_fileStream;
+    long long m_speed;
 
     std::thread m_sendThread;
     std::thread m_useBufThread;
@@ -284,41 +302,74 @@ public:
 
         // 변수들을 초기화한다
         m_currentBufferSize = 0;
-        m_endFlag.store(false);
+        m_endFlag = 1;          // 중지상태
         mp_currentBuffer = &m_buffer1;
         mp_processingBuffer = &m_buffer2;
         m_startTime = 0;
         m_curPlayTime = 0;
         m_endTime = 0;
         m_totalPlayTime = 0;
+        m_speed = 1;
     }
 
     void startReplay()
     {
         // 일시중지였으면 이어서, 정지상태였으면 처음부터 버퍼읽기
         // 해당 구간의 시간 지점부터 데이터를 채워넣는다
-        m_endFlag.store(false);
-        loadFile(m_filePath);
-        // 별도 쓰레드에서 버퍼를 사용시킨다
-        m_useBufThread = std::thread([this] {
-            this->useBuffer();
-        });
-        m_useBufThread.detach();
+        if (m_endFlag == 1)
+        {
+            m_endFlag.store(0);
+            loadFile(m_filePath);
+            // 별도 쓰레드에서 버퍼를 사용시킨다
+            m_useBufThread = std::thread([this] {
+                this->useBuffer();
+            });
+            m_useBufThread.detach();
+            printf("재생 시작\n");
+        }
+        else if(m_endFlag == 2)
+        {
+            m_endFlag.store(0);
+
+            printf("일시정지 해제\n");
+        }
+        else
+        {
+            printf("이미 재생 중\n");
+        }
     }
 
     void stopReplay()
     {
-        m_endFlag.store(true);
+        if (m_endFlag != 1)
+        {
+            m_endFlag.store(1);
+            m_speed = 1;
+            printf("재생 중지\n");
+        }
+        else
+        {
+            printf("이미 중지\n");
+        }
     }
 
     void pauseReplay()
     {
+        if (m_endFlag == 0)
+        {
+            m_endFlag.store(2);
 
+            printf("재생 일시중지\n");
+        }
+        else
+        {
+            printf("재생 중이 아닙니다\n");
+        }
     }
 
     void speedReplay(int _speed)
     {
-
+        m_speed = _speed;           // 1, 2, 4, 8
     }
 
     /*
@@ -424,7 +475,7 @@ public:
             if (!m_fileStream)
             {
                 printf("파일 열기 실패\n");
-                m_endFlag.store(true);
+                m_endFlag.store(1);         // 중지
             }
         }
 
@@ -476,7 +527,7 @@ public:
         else
         {
             // 두 버퍼가 모두 비었으니 재현을 종료한다
-            m_endFlag.store(true);
+            m_endFlag.store(1);         // 중지
         }
     }
 
@@ -486,23 +537,43 @@ public:
     }
 
     void useBuffer() {
-        // (추후: 별도의 쓰레드에서 30ms 마다 동작 타이머타임보다 이전 시간 값인 pReplayBuffer의 값들을 순차적으로 printf 하고)
-        // 테스트용 : 별도 쓰레드에서 30ms마다 큐를 하나씩 꺼내서 printf (원래는 저장과 동일한 사이즈로 맞춰야함)
-        while (m_endFlag != true)
+        // (추후: 별도의 쓰레드에서 32ms 마다 동작 타이머타임보다 이전 시간 값인 pReplayBuffer의 값들을 순차적으로 printf 하고)
+        // 테스트용 : 별도 쓰레드에서 32ms마다 큐를 하나씩 꺼내서 printf (원래는 저장과 동일한 사이즈로 맞춰야함)
+        while (m_endFlag != 1)
         {
-            std::string result;
+            std::string popData;
+            if (m_endFlag == 0)
             {
-                std::lock_guard<std::mutex> lock(m_mutex);
-                if (!mp_currentBuffer->empty())
+                // 추후에는 타이머보다 이하인 시간은 일괄 전송하다가 넘어가면 pop안하고 전송안하는 것으로 넘기기
                 {
-                    std::string popData = mp_currentBuffer->front();
-                    mp_currentBuffer->pop();
-                    sendTask(popData);
+                    std::lock_guard<std::mutex> lock(m_mutex);
+                    if (!mp_currentBuffer->empty())
+                    {
+                        popData = mp_currentBuffer->front();
+                        mp_currentBuffer->pop();
+                        sendTask(popData);
+                        //m_sendThread = std::thread([this, result]() {this->sendTask(result); });
+                        //m_sendThread.detach();
+                    }
                 }
-            }
 
-            //m_sendThread = std::thread([this, result]() {this->sendTask(result); });
-            //m_sendThread.detach();
+                std::this_thread::sleep_for(std::chrono::milliseconds(32 / m_speed));
+            }
+            else if (m_endFlag == 2)
+            {
+                while (m_endFlag == 2)
+                {
+                    // 일시정지
+                    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+                }
+                // 일시정지 해제시
+                // HW장치도 일시정지 해제
+            }
+            else
+            {
+                std::this_thread::sleep_for(std::chrono::milliseconds(500));
+                printf("이건 뭐지??\n");
+            }
 
             if (mp_currentBuffer->empty())
             {
@@ -514,10 +585,11 @@ public:
                 else 
                 {
                     // 둘다 전부 사용했다 (끝)
-                    m_endFlag.store(true);
+                    m_endFlag.store(1);
+                    m_speed = 1;
+                    printf("재생 완료\n");
                 }
             }
-            //std::this_thread::sleep_for(std::chrono::milliseconds(30));
         }
     }
 };
@@ -526,11 +598,11 @@ int main()
 {
     DoubleBufferRecvAndSaveFile* receiver = new DoubleBufferRecvAndSaveFile;
     DoubleBufferFileLoadAndReplay* replayer = new DoubleBufferFileLoadAndReplay;
-    replayer->loadFile("C:\\data\\scenario_1712130547987.dat");
+    replayer->loadFile("C:\\data\\scenario_1712287203219.dat");
 
     while (true) {
         std::string input = "";
-        printf("[0]프로그램 종료, [1]Save시작, [2]Save종료, [3]Replay시작, [4]Replay중지, [5]Replay일시중지, [6]Replay배속2배, [7]Replay구간이동\n");
+        printf("[0]프로그램 종료, [1]Save시작, [2]Save종료, [3]Replay시작, [4]Replay중지, [5]Replay일시중지, [6]Replay배속8배, [7]Replay구간이동, [8]Replay배속1배\n");
         std::getline(std::cin, input);
         if (input == "0")
         {
@@ -558,12 +630,16 @@ int main()
         }
         else if (input == "6")
         {
-            replayer->speedReplay(2);             // 2배속 재생
+            replayer->speedReplay(8);             // 8배속 재생
         }
         else if (input == "7")
         {
-             long long clickTime = 3000;                 // 시작으로부터 3초 후 위치
-            replayer->moveReplay(clickTime);           // 시작시간(ms) + ms - waypoint5개
+            //long long clickTime = 3000;                 // 시작으로부터 3초 후 위치
+            //replayer->moveReplay(clickTime);           // 시작시간(ms) + ms - waypoint5개
+        }
+        else if (input == "8")
+        {
+            replayer->speedReplay(1);             // 1배속 재생
         }
     }
 
