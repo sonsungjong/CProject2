e246718887576f9207688cf36d077bcb840c785b
sonsungjong <tbxmtbfm@naver.com>
2024-05-13 14:01:28 +0900

    엑셀 수정

--- XlsxCLR48/XlsxCLR48.cpp
+++ XlsxCLR48/XlsxCLR48.cpp
@@ -9,31 +9,44 @@ using namespace Microsoft::Office::Interop::Excel;
 
 int main(array<System::String ^> ^args)
 {
-    Application^ excelApp = gcnew ApplicationClass();
-    if (excelApp == nullptr)
+    String^ filePath = "C:\\test\\example4.xlsx";
+    try
     {
-        Console::WriteLine("Excel could not be started. Check that your office installation and project references are correct.");
-        return -1;
+        if (System::IO::File::Exists(filePath))
+        {
+            Console::WriteLine("이미 존재하는 파일명 입니다.");
+        }
+        else
+        {
+            Application^ excelApp = gcnew ApplicationClass();
+            if (excelApp == nullptr)
+            {
+                Console::WriteLine("Excel could not be started. Check that your office installation and project references are correct.");
+                return -1;
+            }
+
+            excelApp->Visible = true;
+
+            // 새 워크북 생성
+            Workbooks^ workbooks = excelApp->Workbooks;
+            Workbook^ workbook = workbooks->Add(Type::Missing);
+            Worksheet^ worksheet = safe_cast<Worksheet^>(workbook->Sheets[1]);
+
+            // 셀에 데이터 입력
+            worksheet->Cells[1, 1] = "Hello";
+            worksheet->Cells[1, 2] = "World";
+
+            // Excel 종료와 정리
+            workbook->SaveAs(filePath, Type::Missing, Type::Missing, Type::Missing, Type::Missing, Type::Missing, XlSaveAsAccessMode::xlNoChange, Type::Missing, Type::Missing, Type::Missing, Type::Missing, Type::Missing);
+            workbook->Close(false, Type::Missing, Type::Missing);
+            excelApp->Quit();
+            delete excelApp;
+        }
     }
-
-    excelApp->Visible = true;
-
-    // 새 워크북 생성
-    Workbooks^ workbooks = excelApp->Workbooks;
-    Workbook^ workbook = workbooks->Add(Type::Missing);
-    Worksheet^ worksheet = safe_cast<Worksheet^>(workbook->Sheets[1]);
-
-    // 셀에 데이터 입력
-    worksheet->Cells[1, 1] = "Hello";
-    worksheet->Cells[1, 2] = "World";
-    worksheet->Cells[1, 3] = "!!!";
-
-    // Excel 종료와 정리
-    String^ filePath = "C:\\test\\example3.xlsx";
-    workbook->SaveAs(filePath, Type::Missing, Type::Missing, Type::Missing, Type::Missing, Type::Missing, XlSaveAsAccessMode::xlNoChange, Type::Missing, Type::Missing, Type::Missing, Type::Missing, Type::Missing);
-    workbook->Close(false, Type::Missing, Type::Missing);
-    excelApp->Quit();
-    delete excelApp;
+    catch (Exception^ e)
+    {
+        Console::WriteLine(e->Message);
+    }
 
     return 0;
 }
