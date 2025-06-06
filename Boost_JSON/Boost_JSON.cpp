adf6354c4625749018914f1a231702a3ca564af9
sonsungjong <tbxmtbfm@naver.com>
2025-06-07 05:41:20 +0900

    부스트 라이브러리 버전 변경

--- Boost_JSON/Boost_JSON.cpp
+++ Boost_JSON/Boost_JSON.cpp
@@ -2,18 +2,18 @@
 // Boost 라이브러리 1.84.0 버전
 /*
 설치
-https://www.boost.org/ -> Download -> boost_1_84_0.zip -> 압축해제
-boost_1_84_0 폴더 -> 관리자 권한으로 cmd 실행 -> .\bootstrap.bat 입력하여 실행
+https://www.boost.org/ -> Download -> boost_1_88_0.zip -> 압축해제
+boost_1_88_0 폴더 -> 관리자 권한으로 cmd 실행 -> .\bootstrap.bat 입력하여 실행
 .\b2.exe 입력하여 빌드
 
-포함 디렉토리 : C:\boost_1_84_0
-라이브러리 디렉토리 : C:\boost_1_84_0\stage\lib
+포함 디렉토리 : C:\boost_1_88_0
+라이브러리 디렉토리 : C:\boost_1_88_0\stage\lib
 필요한 라이브러리를 입력에 넣어야함
 
-라이브러리 입력(릴리즈64) : libboost_json-vc143-mt-x64-1_84.lib
-라이브러리 입력(디버그64) : libboost_json-vc143-mt-gd-x64-1_84.lib
-라이브러리 입력(릴리즈32) : libboost_json-vc143-mt-x32-1_84.lib
-라이브러리 입력(디버그32) : libboost_json-vc143-mt-gd-x32-1_84.lib
+라이브러리 입력(릴리즈64) : libboost_json-vc143-mt-x64-1_88.lib
+라이브러리 입력(디버그64) : libboost_json-vc143-mt-gd-x64-1_88.lib
+라이브러리 입력(릴리즈32) : libboost_json-vc143-mt-x32-1_88.lib
+라이브러리 입력(디버그32) : libboost_json-vc143-mt-gd-x32-1_88.lib
 */
 
 #include <iostream>
@@ -21,10 +21,13 @@ boost_1_84_0 폴더 -> 관리자 권한으로 cmd 실행 -> .\bootstrap.bat 입력하여 실행
 #include <vector>
 #include <boost/json.hpp>
 
+// 라이브러리 중 // 경로로 안되는 경우가 있음
 #ifdef _DEBUG
-#pragma comment(lib, "libboost_json-vc143-mt-gd-x64-1_84.lib")
+#pragma comment(lib, "libboost_container-vc143-mt-gd-x64-1_88.lib")
+#pragma comment(lib, "libboost_json-vc143-mt-gd-x64-1_88.lib")
 #else
-#pragma comment(lib, "libboost_json-vc143-mt-x64-1_84.lib")
+#pragma comment(lib, "libboost_container-vc143-mt-x64-1_88.lib")
+#pragma comment(lib, "libboost_json-vc143-mt-x64-1_88.lib")
 #endif
 
 /*
