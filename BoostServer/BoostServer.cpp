7134963e9208223433dc5989e07288be63002c9e
sonsungjong <tbxmtbfm@naver.com>
2025-07-22 14:53:57 +0900

    1.83

--- BoostServer/BoostServer.cpp
+++ BoostServer/BoostServer.cpp
@@ -1,17 +1,17 @@
 /*
-Boost 라이브러리 1.88.0 버전 설치
-https://www.boost.org/ -> Download -> boost_1_88_0.zip -> 압축해제
-boost_1_88_0 폴더 -> 관리자 권한으로 cmd 실행 -> .\bootstrap.bat 입력하여 실행
+Boost 라이브러리 1.83.0 버전 설치
+https://www.boost.org/ -> Download -> boost_1_83_0.zip -> 압축해제
+boost_1_83_0 폴더 -> 관리자 권한으로 cmd 실행 -> .\bootstrap.bat 입력하여 실행
 .\b2.exe 입력하여 빌드
 
-포함 디렉토리 : C:\boost_1_88_0
-라이브러리 디렉토리 : C:\boost_1_88_0\stage\lib
+포함 디렉토리 : C:\boost_1_83_0
+라이브러리 디렉토리 : C:\boost_1_83_0\stage\lib
 필요한 라이브러리를 입력에 넣어야함
 
-라이브러리 입력(릴리즈64) : libboost_system-vc143-mt-x64-1_88.lib
-라이브러리 입력(디버그64) : libboost_system-vc143-mt-gd-x64-1_88.lib
-라이브러리 입력(릴리즈32) : libboost_system-vc143-mt-x32-1_88.lib
-라이브러리 입력(디버그32) : libboost_system-vc143-mt-gd-x32-1_88.lib
+라이브러리 입력(릴리즈64) : libboost_system-vc143-mt-x64-1_83.lib
+라이브러리 입력(디버그64) : libboost_system-vc143-mt-gd-x64-1_83.lib
+라이브러리 입력(릴리즈32) : libboost_system-vc143-mt-x32-1_83.lib
+라이브러리 입력(디버그32) : libboost_system-vc143-mt-gd-x32-1_83.lib
 */
 #include <iostream>
 #include <deque>
